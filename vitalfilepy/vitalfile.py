1ff5ee90262a4d3a9e570be2f0417c60481871b9
Peter Li <peter0306@gmail.com>
2019-07-20 16:31:23 -0700

    Change encoding from ASCII to utf-8

--- vitalfilepy/vitalfile.py
+++ vitalfilepy/vitalfile.py
@@ -84,21 +84,27 @@ class VitalFile:
     f = None
     filename = ""
     header = None
+    numSamplesInFile = 0    # filesize in byte
 
     def __init__(self, filename: str, mode: str):
         self.filename = filename
         self.mode = mode
         self.header = None
+        self.numSamplesInFile = 0
 
     def open(self):
         if self.mode == "r":
             try:
+                fileSize = os.path.getsize(self.filename)
+                self.numSamplesInFile = int((fileSize - constant.VITALHEADER_SIZE) / (constant.DOUBLE_SIZE * 4))
                 self.f = open(self.filename, "rb")
             except:
                 self.f = None
                 raise VitalFileError("File not found!")
         elif self.mode == "r+":
             try:
+                fileSize = os.path.getsize(self.filename)
+                self.numSamplesInFile = int((fileSize - constant.VITALHEADER_SIZE) / (constant.DOUBLE_SIZE * 4))
                 self.f = open(self.filename, "rb+")
             except:
                 self.f = None
@@ -107,6 +113,7 @@ class VitalFile:
             try:
                 outfile = Path(self.filename)
                 if not outfile.exists():
+                    self.numSamplesInFile = 0
                     self.f = open(self.filename, "wb")
                 else:
                     self.f = None
@@ -121,7 +128,7 @@ class VitalFile:
     def _readStr(self, size: int):
         buf = self.f.read(size)
         # remove the padded '\0'
-        s = buf.decode("ASCII").rstrip('\0')
+        s = buf.decode("utf-8").rstrip('\0')
         return s
 
     def _writeStr(self, s: str, size: int):
@@ -129,7 +136,7 @@ class VitalFile:
         if length > size:
             length = size
         for j in range(length):
-            self.f.write(struct.pack("c", s[j].encode('ASCII')))
+            self.f.write(struct.pack("c", s[j].encode('utf-8')))
         length = size - len(s)
         if length < 0:
             length = 0
@@ -144,7 +151,7 @@ class VitalFile:
         self.header.Label = self._readStr(16)
         self.header.Uom = self._readStr(8)
         self.header.Unit = self._readStr(8)
-        self.header.Bed = self._readStr(4)  
+        self.header.Bed = self._readStr(4)
         self.header.Year, self.header.Month, self.header.Day, self.header.Hour, self.header.Minute = struct.unpack("iiiii", self.f.read(constant.INT32_SIZE * 5))
         self.header.Second = struct.unpack("d", self.f.read(constant.DOUBLE_SIZE))[0]
 
@@ -162,10 +169,30 @@ class VitalFile:
         self.f.write(struct.pack("i", self.header.Minute))
         self.f.write(struct.pack("d", self.header.Second))
 
+
+    def search(self, offsetInSec: double):
+        self._search()
+
+    def getNumSamples(self):
+        (self.fileSize - constant.VITALHEADER_SIZE) / (constant.DOUBLE_SIZE * 4)
+
     # return value, offset, low, high
     def readVitalData(self):
         value, offset, low, high = struct.unpack("dddd", self.f.read(constant.DOUBLE_SIZE * 4))
         return value, offset, low, high
+
+    # return array of tuples of (value, offset, low, high)
+    def readVitalDataBuf(self, numSamples):
+        buf = self.f.read(constant.DOUBLE_SIZE * 4 * numSamples)
+        trunkSize = constant.DOUBLE_SIZE * 4
+        curr = 0
+        arr = []
+        while curr < len(buf):
+            mv = memoryview(buf[curr:curr + trunkSize])
+            t = struct.unpack("dddd", mv)
+            arr.append(t)
+            curr += trunkSize
+        return arr
 
     def writeVitalData(self, value: float, offset: float, low: float, high: float):
         # set to end of file
@@ -174,6 +201,7 @@ class VitalFile:
         self.f.write(struct.pack("d", offset))
         self.f.write(struct.pack("d", low))
         self.f.write(struct.pack("d", high))
+        self.numSamplesInFile += 1
 
     def close(self):
         # print("Close")
